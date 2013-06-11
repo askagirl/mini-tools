89982d51855a34d71dd7af6bd2a6967b5be043ca
Fritz Lee <askagirl@gmail.com>
2013-06-11 09:27:01 +0000

    add fabric install command

--- src/main/resources/bash/fritz-postinstall.sh
+++ src/main/resources/bash/fritz-postinstall.sh
@@ -1,4 +1,14 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install vim curl git tig -y
+# deploy only
+sudo apt-get install python-pip python-dev -y
+sudo pip install fabric fabtools
+
+
+sudo useradd --create-home --password $(perl -e 'print crypt("password", "aa")') askagirl
+sudo useradd --create-home --password $(perl -e 'print crypt("password", "aa")') platform
+
+sudo echo 'platform        ALL=(ALL)       ALL' >> /etc/sudoers
+sudo echo 'askagirl         ALL=(ALL)       NOPASSWD: APACHE' >> /etc/sudoers
 
 mkdir ~/workspaces
